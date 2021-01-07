352868eead43b109affbaf7706b08bcce4137e2c
Rafic El Kouche <raficelkouche@hotmail.com>
2021-01-07 13:38:32 +0000

    add queries

---
+++ 1_queries/5_all_reservations_user.sql
@@ -0,0 +1,8 @@
+SELECT reservations.id, properties.id, avg(rating) as average_rating 
+FROM reservations 
+JOIN properties ON properties.id = reservations.property_id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1 AND end_date < now()::date
+GROUP BY reservations.id, properties.id
+ORDER BY start_date
+LIMIT 10;
